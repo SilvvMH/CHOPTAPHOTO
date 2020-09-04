2b961bd536ed0f4c66a3d92e2bd6bddd8d2271b9
SilvvMH <smahieu@efficom-lille.com>
2020-09-04 22:55:50 +0200

    Update facture-base.php

--- index/INDEXP/facture-base.php
+++ index/INDEXP/facture-base.php
@@ -1,6 +1,71 @@
 <?php
     $id_resa = $_GET['idResa'];
+	$req = "SELECT * FROM reservation WHERE id = $id_resa";
 ?>
+<?php
+
+	$user = array(
+		"id" => 1,
+		"siret" => "152 356 785",
+		"firstname" => "Silvère",
+		"lastname" => "MAHIEU",
+		"email" => "Silvère.mh@outlook.fr",
+		"portable" => "06.25.35.45.35",
+		"address" => "26 Avenue du Bourg\n75000 Paris"
+	);
+
+	$client = array(
+		"id" => 1,
+		"firstname" => "Benoit",
+		"lastname" => "VANDERPLANCKE",
+		"mail" => "bvanderplancke@efficom-lille.com",
+		"portable" => "06.32.23.15.58",
+		"address" => "5 Avenue du Boulevard Maréchal Juin\n14000 Caen",
+	);
+
+	$project = array( //nom de la facture 
+		"id" => 1,
+		"name" => "C",
+		"status" => 1,
+		"infos" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium eos tempora, magni delectus porro cum labore eligendi.",
+		"created" => 1,
+		"paid" => false,
+		"client_id" => 1,
+		"user_id" => 1
+	);
+
+	$tasks[] = array(
+		"id" => 1,
+		"ref" => "96ER1",
+		"description" => "Veille technologique",
+		"price" => 200,
+		"quantity" => 1,
+		"project_id" => 1
+	);
+
+	$tasks[] = array(
+		"id" => 2,
+		"ref" => "152DE",
+		"description" => "Création et intégration d'un thème pour Wordpress",
+		"price" => 500,
+		"quantity" => 1,
+		"project_id" => 1
+	);
+
+	$tasks[] = array(
+		"id" => 3,
+		"ref" => "25365",
+		"description" => "Développement d'un plugin Wordpress sur mesure pour le client",
+		"price" => 1000,
+		"quantity" => 1,
+		"project_id" => 1
+	);
+
+	ob_start();
+	$total = 0;
+	$total_tva = 0;
+?>
+
 <style type="text/css">
 	table {
 		width: 100%;
@@ -42,8 +107,8 @@
 
 	<page_footer>
 		<hr />
-		<p>Fait a Paris, le <?php echo date("d/m/y"); ?></p>
-		<p>Signature du particulier, suivie de la mension manuscrite "bon pour accord".</p>
+		<p>Fait a Lille, le <?php echo date("d/m/y"); ?></p>
+		<p>Copyright &copy; All rights reserved | By @ ChopTaPhoto</p>
 		<p>&nbsp;</p>
 	</page_footer>
 
@@ -64,11 +129,11 @@
 
 	<table style="margin-top: 50px;">
 		<tr>
-			<td class="50p"><h2>Devis n°14</h2></td>
-			<td class="50p" style="text-align: right;">Emis le <?php echo date("d/m/y"); ?></td>
+			<td class="50p"><h2>Facture n°1</h2></td>
+			<td class="50p" style="text-align: right;">Emis le <?php echo date("d/m/yy"); ?></td>
 		</tr>
 		<tr>
-			<td style="padding-top: 15px;" colspan="2"><strong>Objectif:</strong> <?php echo $project['name']; ?></td>
+			<td style="padding-top: 15px;" colspan="2">Commande sur : www.choptaphoto.com</td>
 		</tr>
 	</table>
 
