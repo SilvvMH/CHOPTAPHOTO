e18b33ca4c89710ffb03c4d22b22e43f9f967284
SilvvMH <silvere.mh@outlook.fr>
2021-04-08 14:56:44 +0200

    08/04/2021 - 15h

--- params/FUNCTION/payement.php
+++ params/FUNCTION/payement.php
@@ -4,20 +4,20 @@
         <div class="panel panel-default credit-card-box">
             <div class="panel-heading display-table">
                 <div class="display-tr">
-                    <h3 class="panel-title display-td">Détail du payement</h3>
-                    <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
+                    <h3 class="panel-title display-td">Détail du paiement</h3>
+                    <img class="img-responsive pull-right" src="../../img/payement.jpg">
                 </div>
             </div>
         </div>
         <br>
         <label for="cardNumber">Numéro de carte</label>
         <input type="text" size="10" class="form-control" name="cardNumber" placeholder="0000-0000-0000-0000" autocomplete="cc-number" required autofocus />
-        <label for="cardExpir">Expiration</label>
+        <label for="cardExpir">Date d'expiration</label>
         <input type="text" size="5" class="form-control" name="cardExpiry" placeholder="MM / AA" autocomplete="cc-exp" required />
-        <label for="cardCVC">Code CV</label>
+        <label for="cardCVC">Cryptogramme</label>
         <input type="text" size="3" class="form-control" name="cardCVC" placeholder="CVC" autocomplete="cc-csc" required />
         <br>
-        <button class="blueButton" style="float:right;border-radius: 15px;" type="submit">Confirmez le payement</button>
+        <button class="blueButton" style="float:right;border-radius: 15px;" type="submit">Confirmez le paiement</button>
         <p>&emsp;&emsp;&emsp;&emsp;&emsp;</p>
         <br><br>
 </form>
