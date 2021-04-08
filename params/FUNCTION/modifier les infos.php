32958bf2008ed2a4aaf3ad59afe62dd6cb9137d6
SilvvMH <silvere.mh@outlook.fr>
2021-04-08 16:26:58 +0200

    UPDATE

--- params/FUNCTION/modifier les infos.php
+++ params/FUNCTION/modifier les infos.php
@@ -1,51 +1,48 @@
 <?php
 require("params/BDD/bdd.php");
 
-if (isset($_SESSION['login']))
-{
-    if(isset($_POST['newnom'] , $_POST['newprenom']))
-    {
+if (isset($_SESSION['login'])) {
+    if (isset($_POST['newnom'], $_POST['newprenom'])) {
         $log = $_SESSION['login'];
         $newnom = $_POST['newnom'];
         $newprenom = $_POST['newprenom'];
-        $req = "UPDATE users SET nom = $newnom, prenom = $newprenom WHERE users.$log";
+        $req = $bdd->prepare("UPDATE users SET nom = ?, prenom = ? WHERE login LIKE ? ");
+        $req->execute(array($newnom, $newprenom, $log));
         echo "vous allez etre rediriger vers la page connexion pour que le site prend en compte votre demande.";
         session_destroy();
-        ?>
+?>
         <script>
-        window.setTimeout("location=('index.php?page=connexion');",5000);
+            window.setTimeout("location=('index.php?page=connexion');", 5000);
         </script>
-        <?php
+    <?php
     }
     ?>
     <!-- formulaire -->
     <div class="background">
-            <div class="container">
-                <br>
-                <h2 class="form-group">modifier vos informations :</h2>
-                <form method="POST" action="">
-                    <div class="form-group">
-                        <label for="nom">Nouveau nom :</label>
-                        <input type="text" class="form-control" placeholder="Entrer votre nom" name="newnom">
-                    </div>
-                    <div class="form-group">
-                        <label for="prenom">Nouveau prénom :</label>
-                        <input type="text" class="form-control" placeholder="Entrer votre prénom" name="newprenom">
-                    </div>
-                    <button type="submit" class="btn btn-primary form-group">Enregistrer</button>
-                </form>
-            </div>
+        <div class="container">
             <br>
+            <h2 class="form-group">modifier vos informations :</h2>
+            <form method="POST" action="">
+                <div class="form-group">
+                    <label for="nom">Nouveau nom :</label>
+                    <input type="text" class="form-control" placeholder="Entrer votre nom" name="newnom">
+                </div>
+                <div class="form-group">
+                    <label for="prenom">Nouveau prénom :</label>
+                    <input type="text" class="form-control" placeholder="Entrer votre prénom" name="newprenom">
+                </div>
+                <button type="submit" class="btn btn-primary form-group">Enregistrer</button>
+            </form>
         </div>
+        <br>
+    </div>
     <!-- formulaire -->
-    <?php
-}
-else
-{
-    ?>
+<?php
+} else {
+?>
     <script>
-    window.location.href = "index.php?page=home";
+        window.location.href = "index.php?page=home";
     </script>
-    <?php
+<?php
 }
 ?>
