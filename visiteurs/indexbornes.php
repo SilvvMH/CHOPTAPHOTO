63b3dd3507ba74544b33f715321cfebe597989f6
benoit-vdp <57346192+benoit-vdp@users.noreply.github.com>
2021-04-08 15:29:14 +0200

    Update indexbornes.php

--- visiteurs/indexbornes.php
+++ visiteurs/indexbornes.php
@@ -1,34 +1,30 @@
+
+
+
 <!--script-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 <!--script-->
 
-
-<!-------------background haut du site------------>
-<div class="backgroundhautdusite">
-    <img src="img/FOND-NOIR-PENCHE.png">
-</div>
-<!-------------background haut du site------------>
-
 <!-----------------borne rouge et noir-------------->
 <h2 class="titrebornepersonnalisable2">Les plus simples,</h2>
 <h2 class="titrebornepersonnalisable">Mais les plus demandées !</h2>
-<h2 class="titrebornepersonnalisable2">Laquelle te fait du flash ?</h2>
+<h2 class="titrebornepersonnalisable2">Laquelle te fait flasher ?</h2>
 
 <!--BR-->
 <br>
 <!--BR-->
-
+	
 <!--div container-->
 <div class="containercouleur">
-    <!--div container-->
+<!--div container-->
 
     <!--borne rouge-->
     <figure class="snip0016">
         <img src="img/born2.png" alt="sample42" />
         <figcaption>
             <h2>En Noir :</h2>
-            <p>Les locations de bornes vous coutera 150€/24h par borne et elles sont livrées avec une housse et les cables qui vont avec. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre.</p>
+            <p>Les locations de borne vous couteront 150€/24h par borne et elles sont livrées avec une housse et les câbles qui vont avec. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient.</p>
             <a href="index.php?page=connexion"></a>
         </figcaption>
     </figure>
@@ -39,176 +35,176 @@
         <img src="img/born2rouge.png" alt="sample41" />
         <figcaption>
             <h2>En Rouge :</h2>
-            <p>Les locations de bornes vous coutera 150€/24h par borne et elles sont livrées avec une housse et les cables qui vont avec. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre.</p>
+            <p>Les locations de borne vous couteront 150€/24h par borne et elles sont livrées avec une housse et les câbles qui vont avec. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient.</p>
             <a href="index.php?page=connexion"></a>
         </figcaption>
     </figure>
     <!-- /borne noir-->
-
-    <!-- fin de la div container-->
+	
+<!-- fin de la div container-->
 </div>
 <!-- fin de la div container-->
 
 <!-----------------borne rouge et noir-------------->
 
-<!--BR-->
-<br>
-<br>
-<br>
-<!--BR-->
+    <!--BR-->
+    <br>
+    <br>
+    <br>
+    <!--BR-->
 
-<!------------------bornes réseaux sociaux------------------>
-<div class="containerreseaux flex-mob-col">
-    <div class="presentationtextreseaux">
-        <h2 class="titre">PERSONNALISABLE DE A à Z <span class="badge badge-secondary"> New</span><br> </h2>Tu veux que tes amis, visiteurs ou abonnés
-        <br> voient tout tes réseaux et au passage faire ta PUB
-        <br> pour tout tes réseaux en 1 seul événement.
-        <br> C'est maintenant possible avec la borne réseaux
-        <br> personnalisable de SNAP à TWITTER en passant par INSTA et FACEBOOK !
-        <!--BR-->
-        <br>
-        <br>
-        <!--BR-->
+    <!------------------bornes réseaux sociaux------------------>
+    <div class="containerreseaux flex-mob-col">
+        <div class="presentationtextreseaux">
+            <h2 class="titre">PERSONNALISABLE DE A à Z <span class="badge badge-secondary"> Nouveau</span><br> </h2>Tu veux que tes amis, visiteurs ou abonnés
+            <br> voient tout tes réseaux et au passage faire ta PUB
+            <br> pour tous tes réseaux en 1 seul événement.
+            <br> C'est maintenant possible avec la borne réseaux
+            <br> personnalisable de SNAP à TWITTER en passant par INSTA et FACEBOOK !
+            <!--BR-->
+            <br>
+            <br>
+            <!--BR-->
 
-        <!------------BOUTON RESERVER------------>
-        <div>
-            <a href="index.php?page=connexion">
-                <button type="button" class="btn btn-primary">Me connecter</button>
-            </a>
+            <!------------BOUTON RESERVER------------>
+            <div>
+                <a href="index.php?page=connexion">
+                    <button type="button" class="btn btn-primary">Me connecter</button>
+                </a>
+            </div>
+            <!------------BOUTON RESERVER------------>
+
         </div>
-        <!------------BOUTON RESERVER------------>
-
+        <div class="presentationimagereseaux"><img src="img/bornreseaux.png"></div>
     </div>
-    <div class="presentationimagereseaux"><img src="img/bornreseaux.png"></div>
-</div>
-<!--------------------bornes réseaux sociaux----------------->
+    <!--------------------bornes réseaux sociaux----------------->
 
-<br>
+    <br>
 
-<!-------------------carousel bornes couleurs basic en vente-------------------->
-<h2 class="titrebornepersonnalisable">Les bornes basics</h2>
-<h2 class="titrebornepersonnalisable2">Aux couleurs vives !</h2>
+    <!-------------------carousel bornes couleurs basic en vente-------------------->
+    <h2 class="titrebornepersonnalisable">Les bornes basiques</h2>
+    <h2 class="titrebornepersonnalisable2">Aux couleurs vives !</h2>
 
-<!--BR-->
-<br>
-<br>
-<!--BR-->
+    <!--BR-->
+    <br>
+    <br>
+    <!--BR-->
 
-<!----borne de couleurs---->
+    <!----borne de couleurs---->
 
-<!--div container-->
-<div class="containercouleur flex-mob-col">
+    <!--div container-->
+    <div class="containercouleur flex-mob-col">
     <!--div container-->
 
-    <!--borne jaune-->
-    <figure class="snip00161">
-        <img src="img/bornjaune.png" alt="sample42" />
-        <figcaption>
-            <h2>En Jaune :</h2>
-            <p>Les locations de bornes vous coutera 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre. (intervention sous 5h)</p>
-            <a href="index.php?page=connexion"></a>
-        </figcaption>
-    </figure>
-    <!-- /borne jaune-->
+        <!--borne jaune-->
+        <figure class="snip00161">
+            <img src="img/bornjaune.png" alt="sample42" />
+            <figcaption>
+                <h2>En Jaune :</h2>
+                <p>Les locations de borne vous couteront 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient. (intervention sous 5h)</p>
+                <a href="index.php?page=connexion"></a>
+            </figcaption>
+        </figure>
+        <!-- /borne jaune-->
 
-    <!--borne bleu-->
-    <figure class="snip00161">
-        <img src="img/bornbleu.png" alt="sample41" />
-        <figcaption>
-            <h2>En Bleu :</h2>
-            <p>Les locations de bornes vous coutera 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre. (intervention sous 5h)</p>
-            <a href="index.php?page=connexion"></a>
-        </figcaption>
-    </figure>
-    <!-- /borne bleu-->
+        <!--borne bleu-->
+        <figure class="snip00161">
+            <img src="img/bornbleu.png" alt="sample41" />
+            <figcaption>
+                <h2>En Bleue :</h2>
+                <p>Les locations de borne vous couteront 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient. (intervention sous 5h)</p>
+                <a href="index.php?page=connexion"></a>
+            </figcaption>
+        </figure>
+        <!-- /borne bleu-->
+		
+		<!--borne violette/rose-->
+        <figure class="snip00161">
+            <img src="img/bornviolet.png" alt="sample42" />
+            <figcaption>
+                <h2>En Rose :</h2>
+                <p>Les locations de borne vous couteront 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient. (intervention sous 5h)</p>
+                <a href="index.php?page=connexion"></a>
+            </figcaption>
+        </figure>
 
-    <!--borne violette/rose-->
-    <figure class="snip00161">
-        <img src="img/bornviolet.png" alt="sample42" />
-        <figcaption>
-            <h2>En Rose :</h2>
-            <p>Les locations de bornes vous coutera 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre. (intervention sous 5h)</p>
-            <a href="index.php?page=connexion"></a>
-        </figcaption>
-    </figure>
-
-    <!--borne blanche-->
-    <figure class="snip00161">
-        <img src="img/bornblanche.png" alt="sample41" />
-        <figcaption>
-            <h2>En Blanche :</h2>
-            <p>Les locations de bornes vous coutera 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un probleme se montre. (intervention sous 5h)</p>
-            <a href="index.php?page=connexion"></a>
-        </figcaption>
-    </figure>
+        <!--borne blanche-->
+        <figure class="snip00161">
+            <img src="img/bornblanche.png" alt="sample41" />
+            <figcaption>
+                <h2>En Blanche :</h2>
+                <p>Les locations de borne vous couteront 80€/24h par borne. Un service technique est disponnible 24h/24 7j/7 si pendant votre location un problème survient. (intervention sous 5h)</p>
+                <a href="index.php?page=connexion"></a>
+            </figcaption>
+        </figure>
 
     <!-- fin de la div container-->
-</div>
-<!-- fin de la div container-->
-
-<!--BR-->
-<br>
-<!--BR-->
-
-<!-------------------carousel bornes couleurs basic en vente-------------------->
-
-<!----------------------bornes anniversaires----------------------->
-<div class="container1 flex-mob-col" id="anniv">
-    <div class="presentationtext">
-        <h2 class="titre">LA BORNE POUR TON ANNIVERSAIRE <span class="badge badge-secondary"> Basic</span><br> </h2>C'est la borne qu'il te faut pour ton anniversaire.
-        <br> Tes amis et ta famille vont adorer à 100%.
-        <br> Elle existe par défaut en 2 couleurs : Blanche (avec ta photo) ou noir (avec ta photo),
-        <br> mais nous pouvons réaliser n'importe quelle couleur sur demande (7 jours avant).
-        <br>
-        <br>
-
-        <!--------BOUTON RESERVER-------->
-        <div>
-            <A href="index.php?page=connexion">
-                <button type="button" class="btn btn-primary">Me connecter</button>
-            </A>
-        </div>
-        <!--------BOUTON RESERVER-------->
-
     </div>
-    <div class="presentationimage"><img src="img/bornannivsilv.png"></div>
-</div>
-<!-------------------------bornes anniversaires--------------------->
+    <!-- fin de la div container-->
 
-<br>
-
-<!---------------------texte 4g et connetivité de la borne--------------------->
-<div class="texteconnectivite">
-    <p>
-    <h2 id="entreprise" class="titretextconnect">VOUS ETES UN ENTREPRENEUR ?<br>VOUS VOULLEZ FAIRE VOTRE PUB ?<br>CETTE BORNES ATTIRE LES LIKES ET LES PARTAGES !</h2>
-    <br> Equipée de son domino 4G/3G ou de votre connexion Wifi,
-    <br> on ne peut plus l’arrêter ! les utilisateurs verront votre marque en GARND !
-    <br>Les galeries souvenirs sont crées automatiquement et instantanément
-    <br> également sur le site DROPTAPHOTO que vous pourrez aller voir avec votre code évenement.
-    <br>Vos invités sont encore dans votre événement que les photos sont déjà sur votre compte sur notre site.</p>
+    <!--BR-->
     <br>
-</div>
-<!------------------------texte 4g et connetivité de la borne---------------->
+    <!--BR-->
 
-<!----------------------image borne 2-------------------->
-<div class="container2borne flex-mob-col" id="container2">
-    <div class="presentationimage2borne"><img src="img/bornefficom.png"></div>
-    <div class="presentationtext2borne">
-        <h2 class="titreborne">LA BORNE POUR TON ENTREPRISE ! <span class="badge badge-secondary">basic</span></h2>L'école EFFICOM à Lille nous a fait confiance.
-        <br> Nous lui avons fourni une borne à leur image avec leur logo,
-        <br>les élèves l'ont adorée et ont tous reçu leur photo par la suite.
-        <br>
-        <br>
-        <a href="index.php?page=connexion">
-            <button type="button" class="btn btn-primary btn-lg">Personnaliser votre borne
-                <br>dès maintenant !</button>
-        </a>
+    <!-------------------carousel bornes couleurs basic en vente-------------------->
+
+    <!----------------------bornes anniversaires----------------------->
+    <div class="container1 flex-mob-col" id="anniv">
+        <div class="presentationtext">
+            <h2 class="titre">LA BORNE POUR TON ANNIVERSAIRE <span class="badge badge-secondary"> Basique</span><br> </h2>C'est la borne qu'il te faut pour ton anniversaire.
+            <br> Tes amis et ta famille vont adorer à 100%.
+            <br> Elle existe par défaut en 2 couleurs : Blanche (avec ta photo) ou noir (avec ta photo),
+            <br> mais nous pouvons réaliser n'importe quelle couleur sur demande (7 jours avant).
+            <br>
+            <br>
+
+            <!--------BOUTON RESERVER-------->
+            <div>
+                <A href="index.php?page=connexion">
+                    <button type="button" class="btn btn-primary">Me connecter</button>
+                </A>
+            </div>
+            <!--------BOUTON RESERVER-------->
+
+        </div>
+        <div class="presentationimage"><img src="img/bornannivsilv.png"></div>
     </div>
-</div>
-<!-----------------------image borne 2------------------->
+    <!-------------------------bornes anniversaires--------------------->
 
-<br>
-<br>
+    <br>
+
+    <!---------------------texte 4g et connetivité de la borne--------------------->
+    <div class="texteconnectivite">
+        <p>
+            <h2 id="entreprise" class="titretextconnect">VOUS ETES UN ENTREPRENEUR ?<br>VOUS VOULLEZ FAIRE VOTRE PUB ?<br>CETTE BORNES ATTIRE LES LIKES ET LES PARTAGES !</h2>
+            <br> Equipée de son domino 4G/3G ou de votre connexion Wifi,
+            <br> on ne peut plus l’arrêter ! les utilisateurs verront votre marque en GARND !
+            <br>Les galeries souvenirs sont créées automatiquement et instantanément
+            <br> également sur le site DROPTAPHOTO que vous pourrez aller voir avec votre code évenement.
+            <br>Vos invités sont encore dans votre événement que les photos sont déjà sur votre compte sur notre site.</p>
+        <br>
+    </div>
+    <!------------------------texte 4g et connetivité de la borne---------------->
+
+    <!----------------------image borne 2-------------------->
+    <div class="container2borne flex-mob-col" id="container2">
+        <div class="presentationimage2borne"><img src="img/bornefficom.png"></div>
+        <div class="presentationtext2borne">
+            <h2 class="titreborne">LA BORNE POUR TON ENTREPRISE ! <span class="badge badge-secondary">Basique</span></h2>L'école EFFICOM à Lille nous a fait confiance.
+            <br> Nous lui avons fourni une borne à leur image avec leur logo,
+            <br>les élèves l'ont adorée et ont tous reçu leur photo par la suite.
+            <br>
+            <br>
+            <a href="index.php?page=connexion">
+                <button type="button" class="btn btn-primary btn-lg">Personnaliser votre borne
+                    <br>dès maintenant !</button>
+            </a>
+        </div>
+    </div>
+    <!-----------------------image borne 2------------------->
+
+    <br>
+    <br>
 
 
 </body>
